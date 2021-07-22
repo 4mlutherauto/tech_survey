b51e46f2ee1bedb39e0207b907c5895e68760242
Markki Meyer <markki.meyer@lutherauto.com>
2021-07-21 21:23:38 -0500

    more crud on hours

--- routes/api/hoursRoute.js
+++ routes/api/hoursRoute.js
@@ -6,9 +6,37 @@ router.post('/', (req, res) => {
     Hours.create({
         hours_per_week: req.body.hours_per_week
     })
-    .then((newHours) => {
+        .then((newHours) => {
+            res.json(newHours);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+//get by individual id (primary key)
+router.get('/:id', (req, res) => {
+    Hours.findByPk(req.params.id).then((newHours) => {
         res.json(newHours);
-    })
+    });
+});
+
+//get all--doesn't work unless get/:id is commented out
+// router.get('/all', (req, res) => {
+//     Hours.findAll().then((newHours) => {
+//         res.json(newHours);
+//     });
+// });
+
+router.delete('/:hours_id', (req, res) => {
+    Hours.destroy({
+        where: {
+            hours_id: req.params.hours_id
+        },
+    }
+    ).then((deletedHours) => {
+        res.json(deletedHours);
+})
     .catch((err) => {
         res.json(err);
     });
